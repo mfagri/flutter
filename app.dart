53085a9006aa89378ff8e9e136ff1ec120670ad5
Marouane Fagri <mfagri@e2r5p5.1337.ma>
2022-05-01 16:56:11 +0000

    add class and buttom in center

--- app.dart
+++ app.dart
@@ -10,8 +10,48 @@ import 'package:flutter/material.dart';
 // }
 
 void main() => runApp(MaterialApp(
-  //home: Text('mfgari'),
-  home: Scaffold(
+  home: Home(),
+  // home: Scaffold(
+  //   appBar: AppBar(
+  //     title: Text(
+  //       'mfagri',
+  //        style: TextStyle(
+  //        //backgroundColor: Colors.white,
+  //        fontSize: 20.0,
+  //        fontWeight: FontWeight.bold,
+  //        color: Colors.black
+  //       ),
+  //       ),
+  //     centerTitle: true,
+  //     backgroundColor: Colors.yellow[700],
+  //   ),
+  //   body: Center(
+  //     child: Text(
+  //       'home',
+  //        style: TextStyle(
+  //        //backgroundColor: Colors.white,
+  //        //letterSpacing: 2.0,
+  //        fontSize: 20.0,
+  //        fontWeight: FontWeight.bold,
+  //        color: Colors.black
+  //       ),
+  //     ),
+  //   ),
+  //   // floatingActionButton: FloatingActionButton(
+  //   //   child: Text('Action'),
+  //   // ),
+  //   floatingActionButton: FloatingActionButton(
+	//     onPressed: null,
+	//     child: Text('Action'),
+  //     backgroundColor: Colors.black,
+  //   ),
+  // ),
+));
+// add class and buttom in center
+class  Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     appBar: AppBar(
       title: Text(
         'mfagri',
@@ -42,8 +82,10 @@ void main() => runApp(MaterialApp(
     // ),
     floatingActionButton: FloatingActionButton(
 	    onPressed: null,
-	    child: Text('Action'),
+	    child: Text('click'),
       backgroundColor: Colors.black,
     ),
-  ),
-));
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+  );
+  }
+}
